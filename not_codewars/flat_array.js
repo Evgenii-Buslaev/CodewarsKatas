3ca800992f10f8910bc4dffba61a43a0256beebc
Evgenii Buslaev <eugenebus144@gmail.com>
2022-09-01 22:43:45 +0500

    Solved new kata

---
+++ not_codewars/flat_array.js
@@ -0,0 +1,15 @@
+let arr = [1, 2, [3, 4, [5]], 6, 7, [[8]]]
+
+function flatArray(array) {
+   const newArr = []
+   for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+        newArr.push(...flatArray(array[i]))
+    } else {
+        newArr.push(array[i])
+    }
+   }
+   return newArr
+}
+
+console.log(flatArray(arr));
