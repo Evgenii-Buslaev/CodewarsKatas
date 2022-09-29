4f2458307e0471bdffc02d755a6a1cb1a03be0b0
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-09-29 19:39:43 +0500

    Added short bersion of solution

--- 6kyu/find_the_odd_int.js
+++ 6kyu/find_the_odd_int.js
@@ -9,7 +9,7 @@ Examples
 [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd). */
 
-function findOdd(array) {
+/* function findOdd(array) {
   const pairs = array.reduce((acc, curr) => {
     acc[curr] = (acc[curr] || 0) + 1;
     return acc;
@@ -18,6 +18,10 @@ function findOdd(array) {
   const pair = Object.entries(pairs).filter((pair) => pair[1] % 2 !== 0)[0];
 
   return +pair[0];
+} */
+
+function findOdd(arr) {
+  return arr.find((item) => arr.filter((el) => el == item).length % 2);
 }
 
 console.log(findOdd([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10]));
