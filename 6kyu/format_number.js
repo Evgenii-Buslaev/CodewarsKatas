3533857ec0c559ccfadc7eecf6d7e72a1ae8f9a3
Evgenii Buslaev <eugenebus144@gmail.com>
2022-12-28 19:45:53 +0500

    Solved new kata

---
+++ 6kyu/format_number.js
@@ -0,0 +1,50 @@
+/* Introduction
+You need to write a function that will format a phone number by a template.
+
+Task
+You're given number and string.
+
+If there are more digits than needed, they should be ignored
+
+if there are less digits than needed, should return Invalid phone number
+
+Examples
+(79052479075, "+# ### ### ## ##") => "+7 905 247 90 75"
+(79052479075, "+# (###) ### ##-##") => "+7 (905) 247 90-75"
+(79052479075, "+# ### ### ## ##") => "+7 905 247 90 75"
+(81237068908090, "+## ### ### ## ##") => "+81 237 068 90 80"
+(8123706890, "+## ### ### ##-##") => "Invalid phone number"
+(911, "###") => "911"
+(112, "+ () -") => "+ () -" */
+
+function formatNumber(number, template) {
+  const hashAmount = [...template].reduce((acc, sym) => {
+    if (sym === "#") {
+      acc += 1;
+    }
+    return acc;
+  }, 0);
+
+  const numbers = [...number.toString()].map((num) => {
+    if (!isNaN(+num)) {
+      return num;
+    }
+  });
+
+  if (numbers.length < hashAmount) {
+    return "Invalid phone number";
+  }
+
+  numbers.length = hashAmount;
+
+  const templateArray = [...template];
+  for (let i = 0; i < templateArray.length; i++) {
+    if (templateArray[i] === "#") {
+      templateArray.splice(i, 1, numbers[0]);
+      numbers.shift();
+    }
+  }
+  return templateArray.join("");
+}
+
+console.log(formatNumber(79052479075, "+# ### ### ## ##"));
--- not_codewars/is_palindrom.js
+++ not_codewars/is_palindrom.js
@@ -1,7 +1,16 @@
 /* Напишите функцию, которая проверяет, является ли слово палиндромом. */
 
 function isPalindrome(string) {
-  return string === [...string].reverse().join("");
+  var palindrome = (string) =>
+    string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase() ==
+    string
+      .replace(/[^a-zA-Z0-9]/g, "")
+      .toLowerCase("")
+      .split()
+      .reverse()
+      .join("")
+      ? true
+      : false;
 }
 
 console.log(isPalindrome("тест"));
